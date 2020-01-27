0e5054e910e067b9cc65b8994961ba63a3cd32b5
unknown <sondrelg@live.no>
2020-01-27 19:40:30 +0100

    Added more case check tests

--- tests/unit/test_camel_case.py
+++ tests/unit/test_camel_case.py
@@ -1,14 +1,28 @@
+import pytest
+
+from openapi_tester.case import is_camel_case
+from openapi_tester.exceptions import SpecificationError
 from openapi_tester.utils import camel_case
+
+test_data = [
+    {'input': 'snake_case', 'expected': 'snakeCase'},
+    {'input': 'PascalCase', 'expected': 'pascalCase'},
+    {'input': 'camelCase', 'expected': 'camelCase'},
+    {'input': '', 'expected': ''},
+    {'input': 'lower', 'expected': 'lower'},
+    {'input': 'UPPER', 'expected': 'uPPER'},
+]
 
 
 def test_camel_case():
-    test_data = [
-        {'input': 'snake_case', 'expected': 'snakeCase'},
-        {'input': 'PascalCase', 'expected': 'pascalCase'},
-        {'input': 'camelCase', 'expected': 'camelCase'},
-        {'input': '', 'expected': ''},
-        {'input': 'lower', 'expected': 'lower'},
-        {'input': 'UPPER', 'expected': 'uPPER'},
-    ]
     for item in test_data:
         assert camel_case(item['input']) == item['expected']
+
+
+def test_is_camel_case():
+    for item in test_data:
+        if item['input'] != item['expected']:
+            with pytest.raises(SpecificationError):
+                is_camel_case(item['input'])
+        else:
+            is_camel_case(item['input'])
--- tests/unit/test_snake_case.py
+++ tests/unit/test_snake_case.py
@@ -1,14 +1,28 @@
+import pytest
+
+from openapi_tester.case import is_snake_case
+from openapi_tester.exceptions import SpecificationError
 from openapi_tester.utils import snake_case
+
+test_data = [
+    {'input': 'snake_case', 'expected': 'snake_case'},
+    {'input': 'PascalCase', 'expected': 'pascal_case'},
+    {'input': 'camelCase', 'expected': 'camel_case'},
+    {'input': '', 'expected': ''},
+    {'input': 'lower', 'expected': 'lower'},
+    {'input': 'UPPER', 'expected': 'upper'},
+]
 
 
 def test_snake_case():
-    test_data = [
-        {'input': 'snake_case', 'expected': 'snake_case'},
-        {'input': 'PascalCase', 'expected': 'pascal_case'},
-        {'input': 'camelCase', 'expected': 'camel_case'},
-        {'input': '', 'expected': ''},
-        {'input': 'lower', 'expected': 'lower'},
-        {'input': 'UPPER', 'expected': 'upper'},
-    ]
     for item in test_data:
         assert snake_case(item['input']) == item['expected']
+
+
+def test_is_camel_case():
+    for item in test_data:
+        if item['input'] != item['expected']:
+            with pytest.raises(SpecificationError):
+                is_snake_case(item['input'])
+        else:
+            is_snake_case(item['input'])
