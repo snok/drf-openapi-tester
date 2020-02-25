34f0917957b05834c364ed486b3b18db60cb8ae7
unknown <sondrelg@live.no>
2020-02-25 23:01:04 +0100

    Testing ci

--- openapi_tester/__init__.py
+++ openapi_tester/__init__.py
@@ -1,6 +1,5 @@
-import pkg_resources
-
 from openapi_tester.tester import validate_schema  # noqa: F401
 
-__version__ = pkg_resources.get_distribution('openapi-tester').version
+__version__ = __version__ = '0.0.5'
+__author__ = 'Sondre Lillebø Gundersen'
 __all__ = ['validate_schema']
