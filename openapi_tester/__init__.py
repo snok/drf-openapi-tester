58109665eec1af574063b3f6d7fba37441160ab0
unknown <sondrelg@live.no>
2020-02-25 23:10:23 +0100

    Bumped version

--- openapi_tester/__init__.py
+++ openapi_tester/__init__.py
@@ -1,5 +1,5 @@
 from openapi_tester.tester import validate_schema  # noqa: F401
 
-__version__ = __version__ = '0.0.5'
+__version__ = '0.0.6'
 __author__ = 'Sondre Lillebø Gundersen'
 __all__ = ['validate_schema']
