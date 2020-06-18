f649c661cdd0af29ed58eca562fed0d13b319f97
aa512 <sondre.gundersen@intility.no>
2020-06-18 12:38:18 +0200

    Improves case error messages

--- django_swagger_tester/case/checks.py
+++ django_swagger_tester/case/checks.py
@@ -7,6 +7,16 @@ from django_swagger_tester.exceptions import CaseError
 logger = logging.getLogger('django_swagger_tester')
 
 
+def case_error_message(case: str, key: str) -> str:
+    """
+    Returns an appropriate error message.
+    """
+    return (
+        f'The property `{key}` is not properly {case}\n\n'
+        f'If this is intentional, you can skip case validation by adding `ignore_case=[\'{key}\']` to your function call'
+    )
+
+
 def case_check(case: Union[str, None]) -> Callable[[str, str], None]:
     """
     Returns the appropriate case check based on the `case` input parameter.
@@ -43,12 +53,12 @@ def is_camel_case(key: str) -> None:
         return
     if len(key) == 1 and (key.isalpha() is False or (key.isalpha() is True and key != key.casefold())):
         logger.error('`%s` is not properly camel cased', key)
-        raise CaseError(f'The property `{key}` is not properly camelCased')
+        raise CaseError(case_error_message(key=key, case='camelCased'))
     else:
         camel_cased_key = key[0].lower() + re.sub(r'[\-_.\s]([a-z])', lambda matched: matched.group(1).upper(), key[1:])
         if camel_cased_key != key:
             logger.error('`%s` is not properly camel cased', key)
-            raise CaseError(f'The property `{key}` is not properly camelCased')
+            raise CaseError(case_error_message(key=key, case='camelCased'))
 
 
 def is_snake_case(key: str) -> None:
@@ -64,7 +74,7 @@ def is_snake_case(key: str) -> None:
         return
     if len(key) == 1 and (key.isalpha() is False or (key.isalpha() is True and key != key.casefold())):
         logger.error('%s is not snake cased', key)
-        raise CaseError(f'The property `{key}` is not properly snake_cased')
+        raise CaseError(case_error_message(key=key, case='snake_cased'))
     snake_cased_key = (
         re.sub('([a-z0-9])([A-Z])', r'\1_\2', re.sub('(.)(-)([A-Z][a-z]+)', r'\1_\2', key))
         .lower()
@@ -73,7 +83,7 @@ def is_snake_case(key: str) -> None:
     )
     if snake_cased_key != key:
         logger.error('%s is not snake cased', key)
-        raise CaseError(f'The property `{key}` is not properly snake_cased')
+        raise CaseError(case_error_message(key=key, case='snake_cased'))
 
 
 def is_kebab_case(key: str) -> None:
@@ -89,7 +99,7 @@ def is_kebab_case(key: str) -> None:
         return
     if len(key) == 1 and (key.isalpha() is False or (key.isalpha() is True and key != key.casefold())):
         logger.error('%s is not kebab cased', key)
-        raise CaseError(f'The property `{key}` is not properly kebab-cased')
+        raise CaseError(case_error_message(key=key, case='kebab-cased'))
     kebab_cased_key = (
         re.sub('([a-z0-9])([A-Z])', r'\1-\2', re.sub('(.)([A-Z][a-z]+)', r'\1-\2', key))
         .lower()
@@ -98,7 +108,7 @@ def is_kebab_case(key: str) -> None:
     )
     if kebab_cased_key != key:
         logger.error('%s is not kebab cased', key)
-        raise CaseError(f'The property `{key}` is not properly kebab-cased')
+        raise CaseError(case_error_message(key=key, case='kebab-cased'))
 
 
 def is_pascal_case(key: str) -> None:
@@ -114,11 +124,11 @@ def is_pascal_case(key: str) -> None:
         return
     if len(key) == 1 and (key.isalpha() is False or (key.isalpha() is True and key != key.upper())):
         logger.error('%s is not pascal cased', key)
-        raise CaseError(f'The property `{key}` is not properly PascalCased')
+        raise CaseError(case_error_message(key=key, case='PascalCased'))
     pascal_cased_key = key[0].upper() + re.sub(r'[\-_.\s]([a-z])', lambda matched: matched.group(1).upper(), key[1:])
     if pascal_cased_key != key:
         logger.error('%s is not pascal cased', key)
-        raise CaseError(f'The property `{key}` is not properly PascalCased')
+        raise CaseError(case_error_message(key=key, case='PascalCased'))
 
 
 def skip(*args: Any) -> None:
@@ -128,4 +138,3 @@ def skip(*args: Any) -> None:
     :return: None
     """
     logger.debug('Skipping case validation')
-    pass
