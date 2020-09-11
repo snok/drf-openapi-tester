c9a4f0dc91a7cb64c010df275de43670cd54998e
aa512 <sondre.gundersen@intility.no>
2020-09-11 18:10:06 +0200

    Fixed test broken by pre-commit formatting

--- tests/test_utils/test_format_response_tester_error.py
+++ tests/test_utils/test_format_response_tester_error.py
@@ -44,39 +44,7 @@ Sequence:   test
 
 
 def test_verbose_format():
-    expected = """Item is misspecified:
-
-Summary
-------------------------------------------------------------------------------------
-
-Error:      This is a message
-
-Expected:
-            {
-                "thisIsAVeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryLongKey": "string"
-            }
-Received:
-            {
-                "thisIsAVeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryLongKey": "test"
-            }
-
-Hint:       test
-Sequence:   test
-------------------------------------------------------------------------------------
-
-Response details
-------------------------------------------------------------------------------------
-data          {'thisIsAVeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryLongKey': 'test'}
-type          <class 'dict'>
-------------------------------------------------------------------------------------
-
-Schema
-------------------------------------------------------------------------------------
-title         object_type_title
-type          object
-properties    {'thisIsAVeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryLongKey': {'description': 'This is a string type', 'type': 'string', 'example': 'string'}}
-------------------------------------------------------------------------------------
-"""
+    expected = """Item is misspecified:\n\nSummary\n------------------------------------------------------------------------------------\n\nError:      This is a message\n\nExpected:   \n            {\n                "thisIsAVeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryLongKey": "string"\n            }\nReceived:   \n            {\n                "thisIsAVeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryLongKey": "test"\n            }\n\nHint:       test\nSequence:   test\n------------------------------------------------------------------------------------\n\nResponse details\n------------------------------------------------------------------------------------\ndata          {'thisIsAVeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryLongKey': 'test'}\ntype          <class 'dict'>\n------------------------------------------------------------------------------------\n\nSchema\n------------------------------------------------------------------------------------\ntitle         object_type_title\ntype          object\nproperties    {'thisIsAVeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeryLongKey': {'description': 'This is a string type', 'type': 'string', 'example': 'string'}}\n------------------------------------------------------------------------------------\n"""
     assert format_response_tester_error(error, 'test', verbose=True) == expected
 
 # fmt: on
