25702e2b5ddd62bdb40a57d90d5df8c2c1fdc14e
aa512 <sondre.gundersen@intility.no>
2020-09-11 18:20:49 +0200

    Fixes __all__ to only include real files

--- django_swagger_tester/__init__.py
+++ django_swagger_tester/__init__.py
@@ -2,4 +2,4 @@ default_app_config = 'django_swagger_tester.apps.DjangoSwaggerTesterConfig'
 
 __version__ = '2.0.0'  # Remember to also change pyproject.toml version
 __author__ = 'Sondre Lillebø Gundersen'
-__all__ = ['testing', 'case_testers', 'loaders', 'exceptions', 'middleware', 'wrapper']
+__all__ = ['testing', 'case_testers', 'loaders', 'exceptions']
