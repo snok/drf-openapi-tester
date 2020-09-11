033279d805bd516043bafc591e1fd44f438d9dec
aa512 <sondre.gundersen@intility.no>
2020-09-11 18:14:44 +0200

    Removed dead code

--- demo/api/views/cars.py
+++ demo/api/views/cars.py
@@ -4,7 +4,6 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# from django_swagger_tester.testing import validate
 from ..swagger.auto_schemas import get_cars_auto_schema, get_other_cars_auto_schema
 
 logger = logging.getLogger('django_swagger_tester')
@@ -12,7 +11,6 @@ logger = logging.getLogger('django_swagger_tester')
 
 class GoodCars(APIView):
     @staticmethod
-    # @validate(response=True)
     @get_cars_auto_schema()
     def get(request: Request, **kwargs) -> Response:
         cars = [
@@ -37,11 +35,14 @@ class GoodCars(APIView):
 
 class BadCars(APIView):
     @staticmethod
-    # @validate(response=True)
     @get_other_cars_auto_schema()
     def get(request: Request, **kwargs) -> Response:
         cars = [
-            {'name': 'Saab', 'color': 'Yellow', 'height': 'Medium height',},
+            {
+                'name': 'Saab',
+                'color': 'Yellow',
+                'height': 'Medium height',
+            },
             {'name': 'Volvo', 'color': 'Red', 'width': 'Not very wide', 'length': '2 meters'},
             {'name': 'Tesla', 'height': 'Medium height', 'width': 'Medium width', 'length': '2 meters'},
         ]
