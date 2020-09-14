f519b40f2bdb87bc44d9437d810f50461afa8c4e
aa512 <sondre.gundersen@intility.no>
2020-09-14 15:49:04 +0200

    Got rid of while loop to reduce general risk of loop related bugs

--- django_swagger_tester/loaders.py
+++ django_swagger_tester/loaders.py
@@ -105,7 +105,7 @@ class _LoaderBase:
             )
 
         error = None
-        while True:
+        for _ in route_object.parameters:
             try:
                 route_schema = index_schema(schema=paths_schema, variable=route_object.get_path(), error_addon=route_error)
                 break
