1fc4e7f2e796ac18e6114ca4c011a31c6dd8d4c9
aa512 <sondre.gundersen@intility.no>
2020-09-24 12:39:50 +0200

    Makes cache update correctly

--- django_swagger_tester/selectors.py
+++ django_swagger_tester/selectors.py
@@ -1,6 +1,10 @@
+import logging
 from typing import Optional
 
 from django_swagger_tester.models import Method, Schema, Url, ValidatedResponse
+from django_swagger_tester.utils import resolve_path
+
+logger = logging.getLogger('django_swagger_tester')
 
 
 def save_validated_response(
@@ -9,7 +13,13 @@ def save_validated_response(
     """
     Creates a ValidatedResponse object.
     """
-    url, _ = Url.objects.get_or_create(url=path)
+    # we need to save the deparameterized path (/api/{version}/{vehicle_type}/correct)
+    # and not the path (/api/v1/cars/correct)
+    # otherwise we will get a new DB-entry for every `id` in a /api/v1/resource/{id}-endpoint, which defeats the purpose
+    deparameterized_path, resolved_path = resolve_path(path)
+
+    logger.info('Saving %s response from %s request to `%s`', 'valid' if valid else 'invalid', method, path)
+    url, _ = Url.objects.get_or_create(url=deparameterized_path)
     method, _ = Method.objects.get_or_create(url=url, method=method)
     schema, _ = Schema.objects.get_or_create(hash=str(schema_hash))
     return ValidatedResponse.objects.create(
@@ -21,6 +31,7 @@ def get_validated_response(path: str, method: str, response_hash: str) -> Valida
     """
     Fetches a ValidatedResponse object.
     """
+    deparameterized_path, resolved_path = resolve_path(path)
     return ValidatedResponse.objects.prefetch_related('schema_hash').get(
-        method__url__url=path, method__method=method, response_hash=str(response_hash)
+        method__url__url=deparameterized_path, method__method=method, response_hash=str(response_hash)
     )
