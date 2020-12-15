68189fb98c66e16a86c19579354c1e5727f1d913
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2020-12-15 14:53:18 +0100

    Add APITestCase (#117)

--- tests/test_testers/test_validate_response/test_static.py
+++ tests/test_testers/test_validate_response/test_static.py
@@ -1,45 +1,10 @@
 from django.conf import settings as django_settings
 
+from tests.test_testers.test_validate_response import GOOD_TEST_DATA
+
 from django_swagger_tester.loaders import StaticSchemaLoader
 from django_swagger_tester.testing import validate_response
 
-good_test_data = [
-    {
-        'url': '/cars/correct',
-        'expected_response': [
-            {'name': 'Saab', 'color': 'Yellow', 'height': 'Medium height', 'width': 'Very wide', 'length': '2 meters'},
-            {'name': 'Volvo', 'color': 'Red', 'height': 'Medium height', 'width': 'Not wide', 'length': '2 meters'},
-            {'name': 'Tesla', 'color': 'black', 'height': 'Medium height', 'width': 'Wide', 'length': '2 meters'},
-        ],
-    },
-    {
-        'url': '/trucks/correct',
-        'expected_response': [
-            {'name': 'Saab', 'color': 'Yellow', 'height': 'Medium height', 'width': 'Very wide', 'length': '2 meters'},
-            {'name': 'Volvo', 'color': 'Red', 'height': 'Medium height', 'width': 'Not wide', 'length': '2 meters'},
-            {'name': 'Tesla', 'color': 'black', 'height': 'Medium height', 'width': 'Wide', 'length': '2 meters'},
-        ],
-    },
-]
-
-bad_test_data = [
-    {
-        'url': '/cars/incorrect',
-        'expected_response': [
-            {'name': 'Saab', 'color': 'Yellow', 'height': 'Medium height'},
-            {'name': 'Volvo', 'color': 'Red', 'width': 'Not very wide', 'length': '2 meters'},
-            {'name': 'Tesla', 'height': 'Medium height', 'width': 'Medium width', 'length': '2 meters'},
-        ],
-    },
-    {
-        'url': '/trucks/incorrect',
-        'expected_response': [
-            {'name': 'Saab', 'color': 'Yellow', 'height': 'Medium height'},
-            {'name': 'Volvo', 'color': 'Red', 'width': 'Not very wide', 'length': '2 meters'},
-            {'name': 'Tesla', 'height': 'Medium height', 'width': 'Medium width', 'length': '2 meters'},
-        ],
-    },
-]
 yml_path = str(django_settings.BASE_DIR) + '/openapi-schema.yml'
 
 
@@ -48,8 +13,9 @@ def test_endpoints_static_schema(client, monkeypatch, transactional_db) -> None:
     Asserts that the validate_response function validates correct schemas successfully.
     """
     monkeypatch.setattr(django_settings, 'SWAGGER_TESTER', {'PATH': yml_path, 'SCHEMA_LOADER': StaticSchemaLoader})
-    for item in good_test_data:
-        response = client.get('/api/v1' + item['url'])  # type: ignore
+    for item in GOOD_TEST_DATA:
+        route = f"/api/v1{item['url']}"
+        response = client.get(route)
         assert response.status_code == 200
         assert response.json() == item['expected_response']
-        validate_response(response=response, method='GET', route='/api/v1' + item['url'])  # type: ignore
+        validate_response(response=response, method='GET', route=route)
