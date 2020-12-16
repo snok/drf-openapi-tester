20e7de7f3f5027677ae0c1a6828ff6213ecb2732
Artem Skoretskiy <artem.skoretskiy@gmail.com>
2020-12-16 18:21:28 +0100

    Implement and add tests

--- tests/tester_loaders/test_static_schema_loader/test_load_schema_split.py
+++ tests/tester_loaders/test_static_schema_loader/test_load_schema_split.py
@@ -12,23 +12,22 @@ def test_successful_parse_reference(monkeypatch) -> None:
     base = StaticSchemaLoader()
     base.set_path(yml_split_path)
 
-    with pytest.raises(NotImplementedError):
-        item = base.get_response_schema_section(route="/api/v1/cars/correct", method="GET", status_code=200)
-        assert item == {
+    item = base.get_response_schema_section(route="/api/v1/cars/correct", method="GET", status_code=200)
+    assert item == {
+        'title': 'Success',
+        'type': 'array',
+        'items': {
             'title': 'Success',
-            'type': 'array',
-            'items': {
-                'title': 'Success',
-                'type': 'object',
-                'properties': {
-                    'name': {'description': 'A swedish car?', 'type': 'string', 'example': 'Saab'},
-                    'color': {'description': 'The color of the car.', 'type': 'string', 'example': 'Yellow'},
-                    'height': {'description': 'How tall the car is.', 'type': 'string', 'example': 'Medium height'},
-                    'width': {'description': 'How wide the car is.', 'type': 'string', 'example': 'Very wide'},
-                    'length': {'description': 'How long the car is.', 'type': 'string', 'example': '2 meters'},
-                },
+            'type': 'object',
+            'properties': {
+                'name': {'description': 'A swedish car?', 'type': 'string', 'example': 'Saab'},
+                'color': {'description': 'The color of the car.', 'type': 'string', 'example': 'Yellow'},
+                'height': {'description': 'How tall the car is.', 'type': 'string', 'example': 'Medium height'},
+                'width': {'description': 'How wide the car is.', 'type': 'string', 'example': 'Very wide'},
+                'length': {'description': 'How long the car is.', 'type': 'string', 'example': '2 meters'},
             },
-        }
+        },
+    }
 
 
 def test_successful_parse_external_reference(monkeypatch) -> None:
@@ -38,5 +37,18 @@ def test_successful_parse_external_reference(monkeypatch) -> None:
     base = StaticSchemaLoader()
     base.set_path(yml_split_path)
 
-    with pytest.raises(NotImplementedError):
-        base.get_response_schema_section(route="/api/v1/trucks/correct", method="GET", status_code=200)
+    item = base.get_response_schema_section(route="/api/v1/trucks/correct", method="GET", status_code=200)
+    assert item == {
+        'title': 'Success',
+        'type': 'array',
+        'items': {
+            'type': 'object',
+            'properties': {
+                'name': {'description': 'A swedish truck?', 'type': 'string', 'example': 'Saab'},
+                'color': {'description': 'The color of the truck.', 'type': 'string', 'example': 'Yellow'},
+                'height': {'description': 'How tall the truck is.', 'type': 'string', 'example': 'Medium height'},
+                'width': {'description': 'How wide the truck is.', 'type': 'string', 'example': 'Very wide'},
+                'length': {'description': 'How long the truck is.', 'type': 'string', 'example': '2 meters'},
+            },
+        },
+    }
