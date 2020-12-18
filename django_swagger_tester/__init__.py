13940dde56603d6c7377d57692ecf65135db779b
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2020-12-18 11:25:38 +0100

    Update changelog and bump version

--- django_swagger_tester/__init__.py
+++ django_swagger_tester/__init__.py
@@ -1,5 +1,5 @@
 default_app_config = 'django_swagger_tester.apps.DjangoSwaggerTesterConfig'
 
-__version__ = '2.1.2'  # Remember to also change pyproject.toml version
+__version__ = '2.1.3'  # Remember to also change pyproject.toml version
 __author__ = 'Sondre Lillebø Gundersen'
 __all__ = ['testing', 'case_testers', 'loaders', 'exceptions']
