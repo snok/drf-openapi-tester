12e99eb67c3b71313333cebbc602d22072e3d6e2
Na'aman Hirschfeld <Naaman.Hirschfeld@sprylab.com>
2021-01-17 20:38:04 +0200

    updated tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,9 +1,15 @@
 import json
+import re
 from pathlib import Path
+from typing import List, Optional, Tuple
 
 import yaml
+from django.urls import path
+from django.urls.resolvers import RoutePattern
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from openapi_tester.constants import PARAMETER_CAPTURE_REGEX
 from openapi_tester.schema_converter import SchemaToPythonConverter
 
 CURRENT_PATH = Path(__file__).resolve(strict=True).parent
@@ -18,8 +24,34 @@ def load_schema(file_name: str) -> dict:
             return yaml.load(content, Loader=yaml.FullLoader)
 
 
-def response_factory(schema: dict, path: str, method: str, status_code: int = 200) -> Response:
-    converted_schema = SchemaToPythonConverter(schema, with_faker=True)
+def response_factory(schema: dict, url_fragment: str, method: str, status_code: int = 200) -> Response:
+    converted_schema = SchemaToPythonConverter(schema, with_faker=True).result
     response = Response(status=status_code, data=converted_schema)
-    response.request = dict(REQUEST_METHOD=method, PATH_INFO=path)
+    response.request = dict(REQUEST_METHOD=method, PATH_INFO=url_fragment)
+    response.json = lambda: converted_schema
     return response
+
+
+def url_conf_factory(schema: dict) -> List[RoutePattern]:
+    urlpatterns = []
+    for url_fragment, path_object in schema['paths'].items():
+        parameterized_fragment = url_fragment.replace('{', '<').replace('}', '>')
+        for parameter in list(re.findall(PARAMETER_CAPTURE_REGEX, url_fragment)):
+            parameterized_fragment = parameterized_fragment.replace(parameter, f'{parameter}:{parameter}')
+        urlpatterns.append(path(parameterized_fragment, APIView.as_view()))
+    return urlpatterns
+
+
+def iterate_schema(schema: dict) -> Tuple[Optional[dict], Optional[Response]]:
+    for url_fragment, path_object in schema['paths'].items():
+        for method, method_object in path_object.items():
+            for status_code, responses_object in method_object['responses'].items():
+                schema_section = None
+                response = None
+                if 'content' in responses_object.keys():
+                    schema_section = responses_object['content']['application/json']['schema']
+                elif 'schema' in responses_object.keys():
+                    schema_section = responses_object['schema']
+                if schema_section:
+                    response = response_factory(schema_section, url_fragment, method, status_code)
+                yield schema_section, response
