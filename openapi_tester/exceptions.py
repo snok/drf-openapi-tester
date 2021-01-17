53644f7ea02eab3358031608d8816e688d39c43d
Na'aman Hirschfeld <Naaman.Hirschfeld@sprylab.com>
2021-01-17 08:53:31 +0200

    added an openapi to python converter

--- openapi_tester/exceptions.py
+++ openapi_tester/exceptions.py
@@ -1,7 +1,7 @@
 import json
 from typing import Any
 
-from openapi_tester.constants import OPENAPI_PYTHON_MAPPING
+from openapi_tester.schema_converter import SchemaToPythonConverter
 
 
 class DocumentationError(Exception):
@@ -17,10 +17,11 @@ class DocumentationError(Exception):
         hint: str = '',
         reference: str = '',
     ) -> None:
+        converted_schema = SchemaToPythonConverter(schema or {}).result
         super().__init__(
             self.format(
                 response=self._sort_data(response),
-                example_item=self._sort_data(self.create_dict_from_schema(schema or {})),
+                example_item=self._sort_data(converted_schema),
                 hint=hint,
                 message=message,
                 reference=reference,
@@ -43,7 +44,8 @@ class DocumentationError(Exception):
 
         """
         message = [
-            f'Error: {message}\n\n' f'Expected: {json.dumps(example_item)}\n\n',
+            f'Error: {message}\n\n',
+            f'Expected: {json.dumps(example_item)}\n\n',
             f'Received: {json.dumps(response)}\n\n',
         ]
         if hint:
@@ -53,50 +55,6 @@ class DocumentationError(Exception):
                 f'Sequence: {reference}\n',
             ]
         return ''.join(message)
-
-    def _iterate_schema_dict(self, schema_object: dict) -> dict:
-        parsed_schema = {}
-        if 'properties' in schema_object:
-            properties = schema_object['properties']
-        else:
-            properties = {'': schema_object['additionalProperties']}
-        for key, value in properties.items():
-            if not isinstance(value, dict):
-                raise ValueError()
-            value_type = value['type']
-            if 'example' in value:
-                parsed_schema[key] = value['example']
-            elif value_type == 'object':
-                parsed_schema[key] = self._iterate_schema_dict(value)
-            elif value_type == 'array':
-                parsed_schema[key] = self._iterate_schema_list(value)  # type: ignore
-            else:
-                parsed_schema[key] = OPENAPI_PYTHON_MAPPING[value['type']]
-        return parsed_schema
-
-    def _iterate_schema_list(self, schema_array: dict) -> list:
-        parsed_items = []
-        raw_items = schema_array['items']
-        items_type = raw_items['type']
-        if items_type == 'object':
-            parsed_items.append(self._iterate_schema_dict(raw_items))
-        elif items_type == 'array':
-            parsed_items.append(self._iterate_schema_list(raw_items))
-        else:
-            parsed_items.append(OPENAPI_PYTHON_MAPPING[raw_items['type']])
-        return parsed_items
-
-    def create_dict_from_schema(self, schema: dict) -> Any:
-        """
-        Converts an OpenAPI schema representation of a dict to dict.
-        """
-        schema_type = schema['type']
-        if schema_type == 'array':
-            return self._iterate_schema_list(schema)
-        elif schema_type == 'object':
-            return self._iterate_schema_dict(schema)
-        else:
-            return OPENAPI_PYTHON_MAPPING[schema_type]
 
 
 class CaseError(Exception):
---
+++ openapi_tester/schema_converter.py
@@ -0,0 +1,67 @@
+from typing import Any
+
+from openapi_tester.constants import OPENAPI_PYTHON_MAPPING
+
+
+class SchemaToPythonConverter:
+    """ This class is used both by the DocumentationError format method and the various test suites """
+
+    def __init__(self, schema: dict, with_faker: bool = False):
+        if with_faker:
+            """ We are importing faker here to ensure this remains a dev dependency """
+            from faker import Faker
+
+            Faker.seed(0)
+            self.faker = Faker()
+        schema_type = schema['type']
+        if schema_type == 'array':
+            self.result = self._iterate_schema_list(schema)
+        elif schema_type == 'object':
+            self.result = self._iterate_schema_dict(schema)
+        else:
+            self.result = self._to_mock_value(schema_type)
+
+    def _to_mock_value(self, schema_type: Any) -> Any:
+        if hasattr(self, 'faker'):
+            faker_handlers = {
+                'boolean': self.faker.pybool,
+                'string': self.faker.pystr,
+                'file': self.faker.pystr,
+                'array': self.faker.pylist,
+                'object': self.faker.pydict,
+                'integer': self.faker.pyint,
+                'number': self.faker.pyfloat,
+            }
+            return faker_handlers[schema_type]()
+        else:
+            return OPENAPI_PYTHON_MAPPING[schema_type]
+
+    def _iterate_schema_dict(self, schema_object: dict) -> dict:
+        parsed_schema = {}
+        if 'properties' in schema_object:
+            properties = schema_object['properties']
+        else:
+            properties = {'': schema_object['additionalProperties']}
+        for key, value in properties.items():
+            value_type = value['type']
+            if 'example' in value:
+                parsed_schema[key] = value['example']
+            elif value_type == 'object':
+                parsed_schema[key] = self._iterate_schema_dict(value)
+            elif value_type == 'array':
+                parsed_schema[key] = self._iterate_schema_list(value)  # type: ignore
+            else:
+                parsed_schema[key] = self._to_mock_value(value['type'])
+        return parsed_schema
+
+    def _iterate_schema_list(self, schema_array: dict) -> list:
+        parsed_items = []
+        raw_items = schema_array['items']
+        items_type = raw_items['type']
+        if items_type == 'object':
+            parsed_items.append(self._iterate_schema_dict(raw_items))
+        elif items_type == 'array':
+            parsed_items.append(self._iterate_schema_list(raw_items))
+        else:
+            parsed_items.append(self._to_mock_value(raw_items['type']))
+        return parsed_items
