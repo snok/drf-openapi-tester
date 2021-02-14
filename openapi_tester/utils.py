f266a59bda0aea6c40d60a40903f3dc0fcff5b79
Na'aman Hirschfeld <Naaman.Hirschfeld@sprylab.com>
2021-02-14 20:32:34 +0200

    updated merge logic

--- openapi_tester/utils.py
+++ openapi_tester/utils.py
@@ -7,7 +7,7 @@ def merge_objects(dictionaries: List[Dict[str, Any]]) -> Dict[str, Any]:
     output: Dict[str, Any] = {}
     for dictionary in dictionaries:
         for key, value in dictionary.items():
-            if "allOf" in value:
+            if isinstance(value, dict) and "allOf" in value:
                 all_of = merge_objects(value.pop("allOf"))
                 value = merge_objects([value, all_of])
             if key not in output:
@@ -18,7 +18,6 @@ def merge_objects(dictionaries: List[Dict[str, Any]]) -> Dict[str, Any]:
                 output[key] = list({*output[key], *value})
             if isinstance(current_value, dict) and isinstance(value, dict):
                 output[key] = merge_objects([current_value, value])
-
     return output
 
 
