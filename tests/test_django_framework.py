ff24e32aba3a0b078165ef4cd0382a55a3fc2073
Sondre Lillebø Gundersen <sondrelg@live.no>
2022-11-26 13:44:50 +0100

    chore: Fix linting error

--- tests/test_django_framework.py
+++ tests/test_django_framework.py
@@ -1,9 +1,15 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from django.urls import reverse
-from rest_framework.response import Response
 from rest_framework.test import APITestCase
 
 from openapi_tester import SchemaTester
 from tests.utils import TEST_ROOT
+
+if TYPE_CHECKING:
+    from rest_framework.response import Response
 
 schema_tester = SchemaTester(schema_file_path=str(TEST_ROOT) + "/schemas/sample-schemas/content_types.yaml")
 
